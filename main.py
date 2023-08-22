51174142324d8f63a3272607be5c3ae4c4c4127e
kalimanis <39619795+kalimanis@users.noreply.github.com>
2023-08-22 12:36:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,42 @@
 import pandas as pd
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 import openpyxl
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 # Load the updated data
 updated_df = pd.read_excel('Book1.xlsx')
 
 # Exclude weekends
 updated_df = updated_df[updated_df['Date'].dt.weekday < 5]
 
-# Use the data range for modeling
+# Use the data from January to July for modeling
 train_data_updated = updated_df['TotalCases']
 
 # Define and fit the SARIMA model using the updated training data
-sarima_model_updated = SARIMAX(train_data_updated, order=(1,1,1), seasonal_order=(1,1,1,5))
+sarima_model_updated = SARIMAX(train_data_updated, order=(1, 1, 1), seasonal_order=(1, 1, 1, 5))
 sarima_fit_updated = sarima_model_updated.fit(disp=False)
 
-# Make predictions for month (#n weekdays)
-forecast_updated = sarima_fit_updated.get_forecast(steps=20)
+# Calculate the date range for the next 20 weekdays
+last_date = updated_df['Date'].max()
+next_weekdays = pd.date_range(start=last_date + pd.DateOffset(days=1), periods=20, freq='B')
+
+# Make predictions for the next 20 weekdays
+forecast_updated = sarima_fit_updated.get_forecast(steps=len(next_weekdays))
 forecast_values_updated = forecast_updated.predicted_mean
 confidence_intervals_updated = forecast_updated.conf_int()
 
-# Correct the date range for  weekdays (excluding the weekends)
-august_dates_corrected = pd.bdate_range(start='', end='')[:]  # Take only the first #n weekdays
-
 # Update the forecast dataframe
 forecast_df_updated_corrected = pd.DataFrame({
-    'Date': august_dates_corrected,
+    'Date': next_weekdays,
     'Forecast': forecast_values_updated,
     'Lower CI': confidence_intervals_updated.iloc[:, 0],
     'Upper CI': confidence_intervals_updated.iloc[:, 1]
 })
 
-forecast_df_updated_corrected
-
-import statsmodels.api as sm
-
 # Export the corrected forecast dataframe to an Excel file
-forecast_file_path_updated = "forecasted_data_month.xlsx"
+forecast_file_path_updated = "forecasted_data_next_20_weekdays.xlsx"
 forecast_df_updated_corrected.to_excel(forecast_file_path_updated, index=False)
 
 # Calculate residuals for the updated model
@@ -54,16 +55,44 @@ validation_data = {
 validation_df = pd.DataFrame(validation_data)
 
 # Export the validation data to an Excel file
-validation_file_path = "validation_data_month_2023.xlsx"
+validation_file_path = "validation_data.xlsx"
 validation_df.to_excel(validation_file_path, index=False)
 
-forecast_file_path_updated, validation_file_path
+# Hypothesis and Null Hypothesis
+hypothesis = "H0: The residuals are independently distributed (no autocorrelation)."
+null_hypothesis = "H1: The residuals have significant autocorrelation."
 
-import matplotlib.pyplot as plt
-import seaborn as sns
+# Print the hypothesis and null hypothesis
+print("Hypothesis: ", hypothesis)
+print("Null Hypothesis: ", null_hypothesis)
+
+# Insert an empty line
+print("")
+
+# Validation message
+validation_message = "Model validation: "
+if lb_test_updated['lb_pvalue'].values[0] < 0.05:
+    validation_message += "Not validated (high autocorrelation in residuals)."
+else:
+    validation_message += "Validated (no significant autocorrelation in residuals)."
+
+# Print the validation message
+print(validation_message)
+
+# Valid hypothesis message
+if lb_test_updated['lb_pvalue'].values[0] < 0.05:
+    valid_hypothesis = "The null hypothesis is rejected. Autocorrelation is present in the residuals."
+else:
+    valid_hypothesis = "The null hypothesis is valid. No significant autocorrelation in the residuals."
+
+# Insert an empty line
+print("")
+
+# Print the valid hypothesis message
+print(valid_hypothesis)
+
 
 # Create the plots for residuals, Ljung-Box test, and model information criteria
-
 fig, axes = plt.subplots(3, 1, figsize=(10, 15))
 
 # Plot the residuals
@@ -93,8 +122,6 @@ plt.tight_layout()
 plt.savefig(plot_file_path)
 plt.close()
 
-plot_file_path
-
 # Plot the residuals and the normal Q-Q plot
 fig, axes = plt.subplots(1, 2, figsize=(15, 6))
 
@@ -112,9 +139,9 @@ axes[1].grid(True)
 
 plt.tight_layout()
 
-plot_file_path = "normalqq.png"
+qq_plot_file_path = "normalqq.png"
 plt.tight_layout()
-plt.savefig(plot_file_path)
+plt.savefig(qq_plot_file_path)
 plt.close()
 
-plot_file_path
+forecast_file_path_updated, validation_file_path, plot_file_path, qq_plot_file_path
